8a7eef54a1de4cbaf6d7941e78c3631337767a89
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2023-09-16 10:19:47 +0300

    feat: init collecting data

--- server/index.js
+++ server/index.js
@@ -1,18 +1,26 @@
 import express from 'express';
 import { addHandlers } from './addHandlers.js';
 import { port } from './config/consts.js';
+import { initCollector } from './modules/collect.js';
 
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+function start() {
+  initCollector();
 
-addHandlers(app);
+  const app = express();
 
-app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}`);
-});
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+
+  addHandlers(app);
+
+  app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+  });
+}
+
+start();

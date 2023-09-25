eb0627aeb69b25530572df7cadc2102b6355feee
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2023-09-25 21:23:13 +0300

    feat: create query client data staletime

--- client/src/shared/api/index.ts
+++ client/src/shared/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export * from './server';
-export * from './tanstackQuery';
+export * from './queryClient';
 
 export const api = axios.create({
   baseURL: 'http://localhost:5000',

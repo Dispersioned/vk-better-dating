8cc53df4fd2fce7d7f976a17e4f5861681a91763
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2023-11-01 20:39:11 +0300

    Update getRecommendations.js

--- server/vk-api/getRecommendations.js
+++ server/vk-api/getRecommendations.js
@@ -1,7 +1,7 @@
 import { readStream } from '../utils/readStream.js';
 
 export async function getRecommendations({ token, count, lovinaAgent, sessionKey }) {
-  const res = await fetch('https://dating.vk-apps.ru/api/dating.getRecommendedUsers', {
+  const res = await fetch('https://dating.vk-apps.ru/api/dating.getRecommendedUsersSimple', {
     headers: {
       accept: 'application/json, text/plain, */*',
       'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
